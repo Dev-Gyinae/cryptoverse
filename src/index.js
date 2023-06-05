fe55a35b299ebf9e3c76a295fecae95e0190a384
Gyinae <dev.gyinae@gmail.com>
2023-06-05 11:22:58 +0000

    Initialize project using Create React App

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import App from './App'
+
+
+
+const index = () => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default index
